87ca091b3e18d2a95d5be1cc93fed78f78d7154b
Faiz <faizhassan550@gmail.com>
2024-09-11 23:02:21 +0500

    initial commit

--- MakeViewMOveable/ViewController.swift
+++ MakeViewMOveable/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
-
+ 
 class ViewController: UIViewController {
+
+    @IBOutlet weak var label:UILabel!
+    @IBOutlet weak var draggableView: UIView!
+    private var pangesture: UIPanGestureRecognizer!
+
+    var timer = Timer()
+    var counter = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setDragView()
+        timer.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
     }
 
-
+    @objc func timerAction() {
+        counter += 1
+        label.text = "\(counter)"
+    }
 }
 
+extension ViewController {
+    fileprivate func setDragView() {
+        pangesture = UIPanGestureRecognizer(target: self, action: #selector(draggableFunction(_:)))
+        draggableView.isUserInteractionEnabled = true
+        draggableView.addGestureRecognizer(pangesture)
+    }
+}
+
+extension ViewController {
+    @objc fileprivate func draggableFunction(_ sender: UIPanGestureRecognizer) {
+        draggableView.translatesAutoresizingMaskIntoConstraints = true
+
+        let translation = sender.translation(in: self.view)
+        draggableView.center = CGPoint(x: draggableView.center.x + translation.x, y: draggableView.center.y + translation.y)
+        sender.setTranslation(CGPoint.zero, in: self.view)
+    }
+}
