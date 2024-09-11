9a13828735e9bd5616943cbfff8a4774473b642f
Faiz Ul Hassan <faiz.ul.hassan@sdsoltechnologies.com>
2024-09-11 18:35:40 +0500

    Initial Commit

---
+++ MakeViewMOveable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MakeViewMOveable
+//
+//  Created by Faiz Ul Hassan on 11/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
